0c658b45d22f94dcb094cad7f31ed64b019c4ca8
abhix86 <uchihamadarat99@gmail.com>
2025-10-16 16:28:59 +0530

    correct url implementation

--- src/app.ts
+++ src/app.ts
@@ -4,29 +4,27 @@ import cors from "cors";
 
 const app = Express();
 
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: ["https://socket-io-client-eight.vercel.app/"] }));
 
 const port = 8080;
 
 const server = app.listen(port, () => {
-  console.log(`server is running on http://localhost:8080`);
+  console.log(
+    `server is running on https://socket-io-server-clh8.onrender.com/`
+  );
 });
 
 const io: Server = new Server(server, {
-  cors: { origin: ["http://localhost:5173"] },
+  cors: { origin: ["https://socket-io-client-eight.vercel.app/"] },
 } as any);
 
 io.on("connect", (socket) => {
   socket.emit("welcome", `Connected. ${socket.id}`);
   console.log(`Connection sent to ${socket.id}`);
-  console.log(socket.rooms);
   socket.join("general");
-  console.log(socket.rooms);
 
   socket.on("message", (um) => {
     console.log(um);
     socket.to(um.room).emit("newMessage", um.msg);
   });
-
-  socket.data.username = "Alice";
 });
